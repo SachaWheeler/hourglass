b06f7d421578eeb979ba873abe94a6d68dec9503
Sacha Wheeler <sacha@sachawheeler.com>
2018-09-01 21:19:07 +0100

    x and y axes working.

--- hourglass.ino
+++ hourglass.ino
@@ -4,13 +4,16 @@
 
 const int x_out = A3; // analogue pins
 const int y_out = A2;
-const int z_out = A1;
+const int z_out = A1; // use this for the randomness for slippage constant. flat has low slippage, vertical has high.
 
 const int x_range_low = 230;
 const int x_range_high = 430;
 
 const int y_range_low = 230;
 const int y_range_high = 430;
+const int x_range_mid = (x_range_high + x_range_low) / 2;
+const int y_range_mid = (y_range_high + y_range_low) / 2;
+
 
 const int max_x = 0;
 const int min_x = 300;
@@ -26,6 +29,13 @@ const int D_SW = 6;
 const int D_WEST = 7;
 const int D_NW = 8;
 int quadrant;
+
+const long interval = 1000;
+long previousMillis = 0; 
+unsigned long currentMillis;
+
+const int random_scale = 5;
+int random_score = 1;
 
 /*
  Now we need a LedControl to work with.
@@ -64,14 +74,12 @@ int bit_read(int b, int row, int col){
   return bitRead(board[b][row], 7-col);
 }
 
-void bit_clear(int b, int row, int col){
-  if(col >= 0 && col <= 7 && row >= 0 && row <= 7)
-    bitClear(board[b][row], 7-col);
-}
-
-void bit_set(int b, int row, int col){
+void bit_swap(int from_b, int from_row, int from_col, int b, int row, int col){
+  if(from_col >= 0 && from_col <= 7 && from_row >= 0 && from_row <= 7)
+    bitClear(board[from_b][from_row], 7-from_col);
   if(col >= 0 && col <= 7 && row >= 0 && row <= 7)
     bitSet(board[b][row], 7-col);
+    update_boards();
 }
 
 void process_boards(){
@@ -86,76 +94,93 @@ void process_boards(){
           
           if (quadrant == D_NORTH){
             // check to see if this should pass the particle to the lower board
-            if(b==0 && row==7 && col==7){
-                Serial.print("passing south\n");
-                bit_clear(b, row, col);
-                bit_set(1, 0, 0);
-            }else if(bit_read(b, row+1, col+1) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row+1, col+1);
+            if(b==0 && row==7 && col==7 && bit_read(1, 0, 0) == 0){
+              if(currentMillis - previousMillis > interval) {
+                  previousMillis = currentMillis;   
+
+                  bit_swap(b, row, col, 1, 0, 0);
+              }
+            }else if(bit_read(b, row+1, col+1) == 0){              
+              bit_swap(b, row, col, b, row+1, col+1);
             }else if(bit_read(b, row+1, col) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row+1, col);
+              bit_swap(b, row, col, b, row+1, col);
             }else if(bit_read(b, row, col+1) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row, col+1);
+              bit_swap(b, row, col, b, row, col+1);
             }
           }else if (quadrant == D_NE){
             if(bit_read(b, row+1, col) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row+1, col);
+              bit_swap(b, row, col, b, row+1, col);
+            } // add random slide left or right, if available
+            else if (random(1,random_scale) == random_score){
+              if(bit_read(b, row+1, col-1) == 0){
+                bit_swap(b, row, col, b, row+1, col-1);
+              }else if (bit_read(b, row+1, col+1) == 0){
+                bit_swap(b, row, col, b, row+1, col+1);
+              }
             }
           }else if (quadrant == D_EAST){
             if(bit_read(b, row+1, col-1) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row+1, col-1);
+              bit_swap(b, row, col, b, row+1, col-1);
             }else if(bit_read(b, row, col-1) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row, col-1);
+              bit_swap(b, row, col, b, row, col-1);
             }else if(bit_read(b, row+1, col) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row+1, col);
+              bit_swap(b, row, col, b, row+1, col);
             }
           }else if (quadrant == D_SE){
             if(bit_read(b, row, col-1) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row, col-1);
+              bit_swap(b, row, col, b, row, col-1);
+            } // add random slide left or right, if available
+            else if (random(1,random_scale) == random_score){
+              if(bit_read(b, row-1, col-1) == 0){
+                bit_swap(b, row, col, b, row-1, col-1);
+              }else if (bit_read(b, row+1, col-1) == 0){
+                bit_swap(b, row, col, b, row+1, col-1);
+              }
             }
           }else if (quadrant == D_SOUTH){
             // check to see if this should pass the particle to the upper board   
-            if(b==1 && row==0 && col==0){
-              bit_clear(b, row, col);
-              bit_set(0, 7, 7);
+            if(b==1 && row==0 && col==0 && bit_read(0, 7, 7) == 0){
+               if(currentMillis - previousMillis > interval) {
+                  previousMillis = currentMillis;   
+                  
+                  bit_swap(b, row, col, 0, 7, 7);
+               }
             }else if(bit_read(b, row-1, col-1) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row-1, col-1);
+              bit_swap(b, row, col, b, row-1, col-1);
             }else if(bit_read(b, row-1, col) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row-1, col);
+              bit_swap(b, row, col, b, row-1, col);
             }else if(bit_read(b, row, col-1) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row, col-1);
+              bit_swap(b, row, col, b, row, col-1);
             }
           }else if (quadrant == D_SW){
             if(bit_read(b, row-1, col) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row-1, col);
+              bit_swap(b, row, col, b, row-1, col);
+            } // add random slide left or right, if available
+            else if (random(1, random_scale) == random_score){
+              if(bit_read(b, row-1, col+1) == 0){
+                bit_swap(b, row, col, b, row-1, col+1);
+              }else if (bit_read(b, row-1, col-1) == 0){
+                bit_swap(b, row, col, b, row-1, col-1);
+              }
             }
           }else if (quadrant == D_WEST){
             if(bit_read(b, row-1, col+1) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row-1, col+1);
+              bit_swap(b, row, col, b, row-1, col+1);
             }else if(bit_read(b, row, col+1) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row, col+1);
+              bit_swap(b, row, col, b, row, col+1);
             }else if(bit_read(b, row-1, col) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row-1, col);
+              bit_swap(b, row, col, b, row-1, col);
             }
           }else if (quadrant == D_NW){
             if(bit_read(b, row, col+1) == 0){
-              bit_clear(b, row, col);
-              bit_set(b, row, col+1);
+              bit_swap(b, row, col, b, row, col+1);
+            } // add random slide left or right, if available
+            else if (random(1,random_scale) == random_score){
+              if(bit_read(b, row+1, col+1) == 0){
+                bit_swap(b, row, col, b, row+1, col+1);
+              }else if (bit_read(b, row-1, col+1) == 0){
+                bit_swap(b, row, col, b, row-1, col+1);
+              }
             }
           }
 
@@ -208,8 +233,8 @@ void update_boards(){
 void loop() {
    // put your main code here, to run repeatedly:
   int x_adc_value, y_adc_value, z_adc_value, theta, x, y;
-  int x_range_mid = (x_range_high + x_range_low) / 2;
-  int y_range_mid = (y_range_high + y_range_low) / 2;
+
+  currentMillis = millis();
 
   x = analogRead(x_out);
   x_adc_value = x - x_range_mid;
@@ -233,53 +258,5 @@ void loop() {
   else                              quadrant = D_NW;
 
   process_boards();
-
-  update_boards();
-
-  Serial.print("theta = ");
-  Serial.print(theta);
-  Serial.print("\t\t");
-  Serial.print("quadrant = ");
-  Serial.print(quadrant);
-  Serial.print("\n");
- 
-  delay(10);
 }
-
-/*
- * 
-  flat    : x = 331    y = 324   z = 411
-  reversed: x = 331    y = 327   z = 278
-  
-  vertical: x = 396    y = 326   z = 356
-  90      : x = 335    y = 390   z = 364
-  inverted: x = 265    y = 327   z = 355
-  270     : x = 330    y = 262   z = 353
-
-  x_range: high: 396 mid: 330 low: 265
-  y_range: high: 390 mid: 326 low: 262
-
-  prototype:
-    populate 60 particles in 64 squares
-    find which direction is *down*
-    shunt particles in that direction
-
-    def what direction is down(x, y):
-      // theta = tan–1(y/x)
-      // https://www.dummies.com/education/science/physics/how-to-find-a-vectors-magnitude-and-direction/
-      
-
-  setup:
-    create 2 boards of 64 sqaures
-    fill first board with 60 particles randomly
-
-  loop:
-
-    find which direction is down
-    shunt each particle into an open space *down* (up, doww, left, right & diagonal)
-
-    if down is laterally diagonal and gateway space is occupied:
-      move gateway particle into southern square
-      
-*/
 
